d79d2c8f0e711af9bc1661134cb886c5144b6325
Arthur Moreira Barros <mb.arthurmiguel@gmail.com>
2025-07-19 20:38:28 -0300

    Conclusão da página e preparativos finais para a hospedagem.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const buttonToggle = document.querySelector('.buttib-toggle')
-const sidebar = document.querySelector('.sidebar')
+const buttonToggle = document.querySelector('.button-toggle')
+const pagebar = document.querySelector('.pagebar')
 const switchCircle = document.querySelector('.theme-switch span')
+
+buttonToggle.addEventListener('click', () => {
+    pagebar.classList.toggle('hidden-bar')
+    buttonToggle.classList.toggle('show-bar')
+})
 
 switchCircle.addEventListener('click', () => {
     switchCircle.classList.toggle('right-placed')
+    document.documentElement.classList.toggle('dark-root')
 
     const switchIcon = switchCircle.querySelector('i')
     switchIcon.classList.contains('bi-brightness-low-fill')
