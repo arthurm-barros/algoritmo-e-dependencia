ab6f7c35fba6748de02ceb4856dee658aceaa862
Arthur Moreira Barros <mb.arthurmiguel@gmail.com>
2025-07-19 22:33:38 -0300

    Commit final.

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const buttonToggle = document.querySelector('.button-toggle')
 const pagebar = document.querySelector('.pagebar')
 const switchCircle = document.querySelector('.theme-switch span')
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.body.clientWidth <= 850) {
+        buttonToggle.className = 'button-toggle show-bar'
+        pagebar.className = 'pagebar hidden-bar'
+    }
+})
+
 buttonToggle.addEventListener('click', () => {
     pagebar.classList.toggle('hidden-bar')
     buttonToggle.classList.toggle('show-bar')
